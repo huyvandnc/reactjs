83a446cea5cc61dd1e9c4eca5d4449ab907d9104
huyvandnc <huyvan.dnc@gmail.com>
2020-01-23 21:43:44 +0700

    First Commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
-import { logger } from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import reducer from './reducers';
 import thunk from 'redux-thunk';
+const logger = createLogger({
+    collapsed: true
+});
 export const store = createStore(reducer, applyMiddleware(thunk, logger));
