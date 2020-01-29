2f9c919277d53e23bd718f4ae1c308f15c8f250e
Huy Văn <huyvan.dnc@gmail.com>
2020-01-29 22:35:36 +0700

    First Commit

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -5,8 +5,11 @@ import {
     Link,
     TextField,
     Button,
-    Grid
+    Grid,
+    Avatar,
+    CssBaseline
 } from '@material-ui/core';
+import { LockOutlined } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { userActions } from '../../redux/actions';
@@ -57,30 +60,38 @@ const Wrapper = styled.div`
    flex-direction: column;
 `;
 
+const handleLogin = (event) => {
+
+}
+
 const Login = (props) => {
     const classes = useStyles();
     const [email, setEmail] = useState('');
-    const [pasword, setPasword] = useState('');
+    const [password, setPasword] = useState('');
 
     return (
-        <Wrapper>
-            <Header {...props}/>
-            <Container component="main" maxWidth="xs">
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlined />
+                </Avatar>
+                <Typography component="h1" variant="h5">Đăng Nhập</Typography>
                 <form className={classes.form} noValidate>
-                <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus/>
-                <TextField variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password"/>
-                <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>Sign In</Button>
-                <Grid container>
-                    <Grid item xs>
-                      <Link href="#" variant="body2">Forgot password?</Link>
+                    <TextField value={email} size="small" variant="outlined" margin="normal" required fullWidth id="email" label="Email" name="email" autoComplete="email" autoFocus/>
+                    <TextField value={password} size="small" variant="outlined" margin="normal" required fullWidth name="password" label="Mật khẩu" type="password" id="password" autoComplete="current-password"/>
+                    <Button type="submit" disableElevation fullWidth variant="contained" color="primary" className={classes.submit}>Đăng nhập</Button>
+                    <Grid container>
+                        <Grid item xs>
+                        <Link href="#" variant="body2">Quên mật khẩu?</Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">{"Chưa có tài khoản? Đăng ký"}</Link>
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <Link href="#" variant="body2">{"Don't have an account? Sign Up"}</Link>
-                    </Grid>
-                </Grid>
-                 </form>
-            </Container>
-        </Wrapper>
+                </form>
+            </div>
+        </Container>
     );
 }
 
